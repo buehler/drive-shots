6f7f4122a2212df5bdbdb63786da372b95e3539b
Christoph Bühler <christoph@smartive.ch>
2018-10-12 12:47:38 +0200

    add drive upload

--- src/drive-shots.ts
+++ src/drive-shots.ts
@@ -2,10 +2,11 @@ import { app } from 'electron';
 import { injectable } from 'inversify';
 
 import { TrayMenu } from './menu/tray-menu';
+import { DriveUploader } from './uploader/drive-uploader';
 
 @injectable()
 export class DriveShots {
-  constructor(_tray: TrayMenu) {}
+  constructor(_tray: TrayMenu, _uploader: DriveUploader) {}
 
   public start(): void {
     app.dock && app.dock.hide();
--- src/ioc.ts
+++ src/ioc.ts
@@ -14,21 +14,50 @@ import { HistoryDetector } from './history/history-detector';
 import { IocSymbols } from './ioc-symbols';
 import { AppFolderOpener } from './menu/app-folder-opener';
 import { TrayMenu } from './menu/tray-menu';
+import { DriveUploader } from './uploader/drive-uploader';
 import { UrlShortener } from './url-shortener/smrtv-shortener';
 import { AutoUpdater } from './utils/auto-updater';
 
 const ioc = new Container();
 
-ioc.bind(AppFolderOpener).to(AppFolderOpener);
-ioc.bind(Assets).to(Assets);
-ioc.bind(Authenticator).to(Authenticator);
-ioc.bind(AutoUpdater).to(AutoUpdater);
+ioc
+  .bind(AppFolderOpener)
+  .to(AppFolderOpener)
+  .inSingletonScope();
+ioc
+  .bind(Assets)
+  .to(Assets)
+  .inSingletonScope();
+ioc
+  .bind(Authenticator)
+  .to(Authenticator)
+  .inSingletonScope();
+ioc
+  .bind(AutoUpdater)
+  .to(AutoUpdater)
+  .inSingletonScope();
 ioc.bind(drive_v3.Drive).toConstantValue(google.drive('v3'));
-ioc.bind(DriveShots).to(DriveShots);
-ioc.bind(HistoryDetector).to(HistoryDetector);
+ioc
+  .bind(DriveShots)
+  .to(DriveShots)
+  .inSingletonScope();
+ioc
+  .bind(DriveUploader)
+  .to(DriveUploader)
+  .inSingletonScope();
+ioc
+  .bind(HistoryDetector)
+  .to(HistoryDetector)
+  .inSingletonScope();
 ioc.bind<JsonConfig>(IocSymbols.config).toConstantValue(jsonConfig);
-ioc.bind(TrayMenu).to(TrayMenu);
-ioc.bind(UrlShortener).to(UrlShortener);
+ioc
+  .bind(TrayMenu)
+  .to(TrayMenu)
+  .inSingletonScope();
+ioc
+  .bind(UrlShortener)
+  .to(UrlShortener)
+  .inSingletonScope();
 
 // Windows bindings
 
