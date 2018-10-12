877174a0c1e4e1f316988684831dbe06d9950500
Christoph Bühler <christoph@smartive.ch>
2018-10-12 12:42:58 +0200

    add url shortener

--- src/url-shortener/smrtv-shortener.ts
+++ src/url-shortener/smrtv-shortener.ts
@@ -1,8 +1,19 @@
+import { put, Response } from 'got';
 import { injectable } from 'inversify';
+
+const shortenerUrl = 'https://smr.tv/api/links';
 
 @injectable()
 export class UrlShortener {
   public async shorten(url: string): Promise<string> {
-    return url;
+    const shortUrl = (await put(shortenerUrl, {
+      json: true,
+      body: { url },
+      headers: {
+        Authorization:
+          'ApiKey UaCDZm3Z1W63likKTQ6BlgAS7W892hMPSq5PtEjJ8bUqYvPtU01zUdQE6IEna9ML',
+      },
+    })) as Response<{ url: string; shortUrl: string }>;
+    return shortUrl.body.shortUrl;
   }
 }
